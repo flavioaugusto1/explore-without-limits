521ad66a156a8e49785a63b24b11d7f0f8539c92
Flávio <f.augustosdn@gmail.com>
2024-01-18 15:43:05 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <section>
+        <img src="" alt="" />
+        <div className="titles">
+          <h1></h1>
+          <p></p>
+          <img src="" alt="" />
+        </div>
+      </section>
+    </Container>
+  );
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    PURPLE: "#312D64",
+    PURPLE_LINEAR: "linear-gradient(96deg, #343193 0%, #302C63 99.68%);",
+
+    PINK: "#B52184",
+
+    WHITE_BG: "#F9F4FB",
+    WHITE: "#FFFFFF",
+  },
+};
